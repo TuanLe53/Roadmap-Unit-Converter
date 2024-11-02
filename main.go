53539a652622428809171397c0e6ed1f52742fa5
Tuan Le <leanhtuan030523@gmail.com>
2024-11-02 16:45:59 +0700

    Remove '/ping' route, '/' now return index.html

--- main.go
+++ main.go
@@ -11,10 +11,8 @@ func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main website",
-		})
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
 	log.Fatal(r.Run())
