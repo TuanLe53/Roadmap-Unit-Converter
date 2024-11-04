4888a09e973b3b237e789ce669d1eef3c96c47b1
Tuan Le <leanhtuan030523@gmail.com>
2024-11-04 13:14:24 +0700

    Remove '/result' route, add '/convert/length' route

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,8 +28,29 @@ func main() {
 		c.HTML(http.StatusOK, "temperature.html", nil)
 	})
 
-	r.GET("/result", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "result.html", nil)
+	r.POST("/convert/length", func(c *gin.Context) {
+		source_value := c.PostForm("source_value")
+		source_unit := c.PostForm("source_unit")
+		target_unit := c.PostForm("target_unit")
+
+		value, err := strconv.ParseFloat(source_value, 64)
+		if err != nil {
+			fmt.Println("Error converting string to float64:", err)
+		}
+
+		target_value, err := convertLength(value, source_unit, target_unit)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		c.HTML(http.StatusOK, "result.html", gin.H{
+			"conversion_type": "length",
+			"source_unit":     source_unit,
+			"target_unit":     target_unit,
+			"source_value":    source_value,
+			"target_value":    target_value,
+		})
 	})
 
 	log.Fatal(r.Run())
